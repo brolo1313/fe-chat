8337a6065cc3fe5257c14f34bf1a75c4545e48cd
Andrew Yupin <brolo1341@gmail.com>
2025-05-08 13:46:06 +0300

    fix: update webUri in environment files for production and development configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: true,
+  webUri: 'https://be-chat-qynz.onrender.com',
   apiUrl: 'https://be-chat-qynz.onrender.com/api',
   socketUrl: 'https://be-chat-qynz.onrender.com',
   googleClientId: '64531276471-rsgodsdm2879qssn0kvo3pkmtni0q1d5.apps.googleusercontent.com',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  webUri: 'http://localhost:4202',
   apiUrl: 'http://localhost:3000/api',
   socketUrl: 'http://localhost:3000',
   googleClientId: '64531276471-rsgodsdm2879qssn0kvo3pkmtni0q1d5.apps.googleusercontent.com',
