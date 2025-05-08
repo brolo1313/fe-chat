20dc4a1e81d39e0884df0f6c518e1b4b9111e56a
Andrew Yupin <brolo1341@gmail.com>
2025-05-08 13:23:25 +0300

    updated environment configuration for production and development; ensure correct API and socket URLs are set

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 
 export const environment = {
-    production: true,
-    // apiUrl: 'https://node-implementation.vercel.app/api',
-  };
+  production: true,
+  apiUrl: 'https://be-chat-qynz.onrender.com/api',
+  socketUrl: 'https://be-chat-qynz.onrender.com',
+  googleClientId: '64531276471-rsgodsdm2879qssn0kvo3pkmtni0q1d5.apps.googleusercontent.com',
+};
 
-  
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api',
-    socketUrl: 'http://localhost:3000',
-    googleClientId: '64531276471-rsgodsdm2879qssn0kvo3pkmtni0q1d5.apps.googleusercontent.com',
-  };
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  socketUrl: 'http://localhost:3000',
+  googleClientId: '64531276471-rsgodsdm2879qssn0kvo3pkmtni0q1d5.apps.googleusercontent.com',
+};
 
-  
